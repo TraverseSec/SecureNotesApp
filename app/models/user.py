164c2d557ca1346107af3f8b4504c47d229844e6
TraverseSec <traveleracrossrealms@gmail.com>
2026-04-28 12:59:40 -0700

    Created user model

---
+++ app/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, DateTime, func
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    hashed_password = Column(String(255), nullable=False)
+
